dd2e581176ef9a088a706092ceac996c51a8559d
William Bello <willjobs94@gmail.com>
2015-08-02 11:12:59 -0400

    deleted some unused files

--- Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$url = 'http://' . $_SERVER['HTTP_HOST'];            
-	$url .= rtrim(dirname($_SERVER['PHP_SELF']), '/\\'); 
-	$url .= '/views/layout.php';           
-	header('Location: ' . $url, true, 302);
-	?>
